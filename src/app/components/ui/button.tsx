b0ec7fca13e15091ae11cf789dd4ecc31b99ba41
DuHNunes <duhnunes.dev@gmail.com>
2025-04-16 11:42:07 -0300

    feat: Add modal to add a game to list

--- src/app/components/ui/button.tsx
+++ src/app/components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
         outline:
           'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
         secondary:
-          'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
+          'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/70',
         ghost:
           'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
         link: 'text-primary underline-offset-4 hover:underline',
@@ -25,9 +25,7 @@ const buttonVariants = cva(
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
         xs: 'py-1 has-[>svg]:px-2',
-        sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
-        lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
-        icon: 'size-9',
+        full: 'w-full p-2',
       },
     },
     defaultVariants: {
--- src/app/features/menu/Menubar.tsx
+++ src/app/features/menu/Menubar.tsx
@@ -1,6 +1,15 @@
 import { LuGamepad2, LuListCheck, LuListPlus } from 'react-icons/lu'
 
 import { Button } from '@/app/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/app/components/ui/dialog'
 import { Input } from '@/app/components/ui/input'
 import { Separator } from '@/app/components/ui/separator'
 import { TabsList, TabsTrigger } from '@/app/components/ui/tabs'
@@ -9,10 +18,29 @@ export function Menubar() {
   return (
     <nav className="p-2 px-3 w-full flex items-center shrink-0 border-b border-border/15 overflow-hidden">
       <div className="flex">
-        <Button size="xs">
-          <LuListPlus className="size-5" />
-        </Button>
-        <Input placeholder="Search..." />
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="xs">
+              <LuListPlus className="size-5" />
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Novo jogo</DialogTitle>
+              <DialogDescription>Adicione um jogo novo</DialogDescription>
+            </DialogHeader>
+            <Input placeholder="Game name" />
+            <section className="flex gap-2 border border-border/20 p-2 rounded min-h-38 text-muted-foreground">
+              Add one game
+            </section>
+            <DialogFooter>
+              <Button size="full" variant="secondary">
+                Adicionar
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+        <Input placeholder="Search game" />
       </div>
       <Separator orientation="vertical" />
       <TabsList>
